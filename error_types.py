bec13476cc6f120de836e44db3a0991cc1e0f692
philcombiths <philcombiths@gmail.com>
2020-08-03 15:47:05 -0700

    Working update
    Working version. Idiosyncratic issues are labelled as "other" or "substitution_other"

--- error_types.py
+++ error_types.py
@@ -21,7 +21,7 @@ Setup Procedures:
     (e.g., 'C:/Users/Philip/Anaconda3/Lib/site-packages/panphon/data')
     
 Example use case:
-result = error_patterns_table("G:\My Drive\Phonological Typologies Lab\Projects\Spanish SSD Tx\Data\Processed\ICPLA 2020_2021\SpTxR\microdata_b.csv")            
+result = error_patterns_table("G:\My Drive\Phonological Typologies Lab\Projects\Spanish SSD Tx\Data\Processed\ICPLA 2020_2021\SpTxR\microdata_c.csv")            
     
 Created on Tue Jul 28 14:58:21 2020
 @author: Philip Combiths
@@ -29,6 +29,7 @@ Created on Tue Jul 28 14:58:21 2020
 
 import pandas as pd
 from collections import OrderedDict
+import io
 import regex as re
 import panphon
 ft = panphon.FeatureTable()
@@ -90,7 +91,7 @@ def extract_diacritics(join=True):
     return list(set(res))
     
 
-def error_pattern(target, actual):    
+def error_pattern(target, actual, debug=False):    
     """
     Return the phonological error pattern from a target/actual pair of
     consonants or consonant sequences.
@@ -104,8 +105,14 @@ def error_pattern(target, actual):
     
     *Currently only tested for reliability with C and CC clusters.
     *CCC cluster epenthesis requires revision.
+    
+    TO DO: Could determine instances that still count as "present" instead of substitution:
+        e.g., "kʴ" for "kr".
+    TO DO: Specify vocalization substitution pattern
+        
     """   
     error = None
+    
     ############# 
     # Workarounds    
     # Sub ᵊ with ə for easier epenthesis ID
@@ -121,14 +128,15 @@ def error_pattern(target, actual):
     if actual == 'nan':
         error = "deletion"
         return error
-    #############       
-    # Generate segments and features with panphon
+    #############     
+    
+    # Generate segments and features from panphon
     t_segs = ft.ipa_segs(target)
     t_fts = ft.word_fts(target)
     a_segs = ft.ipa_segs(actual)
     a_fts = ft.word_fts(actual)
     t_dict = OrderedDict(zip(t_segs, t_fts))
-    a_dict = OrderedDict(zip(a_segs, a_fts))
+    a_pairs = list(zip(a_segs, a_fts))
                     
     # Deletion
     if a_segs == ['∅'] or len(a_segs) == 0:
@@ -155,195 +163,81 @@ def error_pattern(target, actual):
         else:
             error = "other"
             return error
-               
-    if len(target) == 2:
-        structure = "CC"
-   
-        # Epenthesis
-        for seg in a_dict:       
-            if ('syl', 1) in a_dict[seg].items() and len(a_dict) > 2:
-                # Contains a vowel
-                error = 'epenthesis'            
-                for seg in a_segs:
-                    if ('syl', 1) in a_dict[seg].items():
-                        continue
-                    else:
-                        if seg in t_segs:
-                            if t_segs.index(seg) == 0:
-                                error = error+'-C1pres'
-                            if t_segs.index(seg) == 1:
-                                error = error+'-C2pres'
-                            if t_segs.index(seg) == 2:
-                                error = error+'-C3pres'    
-                        else:        
-                            index = 0
-                            smallest_dist = (index, 1)
-                            for t_ft in t_fts:                        
-                                dist = t_ft-a_dict[seg]                            
-                                if dist < smallest_dist[1]:
-                                    if t_segs[index] not in a_segs:
-                                        smallest_dist = (index, dist)
-                                index += 1
-                            if smallest_dist[0] == 0:
-                                error = error+'-C1sub'
-                            if smallest_dist[0] == 1:
-                                error = error+'-C2sub'
-                            if smallest_dist[0] == 2:
-                                error = error+'-C3sub'                        
-                error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
-                return error
-       
-        # Cluster Reduction
-        if len(a_segs) == len(t_segs)-1:
-            error = "reduction"
-            for seg in a_segs:
-                if seg in t_segs:
-                    if t_segs.index(seg) == 0:
-                        error = error+'-C2del'
-                    if t_segs.index(seg) == 1:
-                        error = error+'-C1del'
-                else:
-                    index = 0
-                    smallest_dist = (index, 1)
-                    for feat in t_fts:
-                        dist = feat-a_fts[0]
-                        if dist < smallest_dist[1]:
-                            smallest_dist = (index, dist)
-                        index += 1
-                    if smallest_dist[0] == 0:
-                        error = error+'-C2del-C1sub'
-                    if smallest_dist[0] == 1:
-                        error = error+'-C1del-C2sub'
-            if 'C1' not in error:
-                error = error+'-C1pres'                        
-            if 'C2' not in error:
-                error = error+'-C2pres'
-            error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
-            return error
-                        
-        # Substitution
-        if len(a_segs) == len(t_segs):
-            error = 'substitution'
-            if a_segs[0] == t_segs[0]:
-                if a_segs[1] != t_segs[1]:
-                    error = error+'-C2sub'
-                else:
-                    error = error+'-C1sub-C2sub'
-            else:
-                if a_segs[1] != t_segs[1]:
-                    error = error+'-C1sub-C2sub'
-                else:
-                    error = error+'-C1sub'
-            if 'C1' not in error:
-                error = error+'-C1pres'                        
-            if 'C2' not in error:
-                error = error+'-C2pres'
-            error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
-            return error
-        
-        # All other errors
-        else:
-            error = "other"
-            return error
                 
-    if len(target) == 3:
-        structure = "CCC"
-        
+    if 2 <= len(target) <= 3 :        
         # Epenthesis
-        for seg in a_dict:       
-            if ('syl', 1) in a_dict[seg].items():
+        for seg in a_pairs:       
+            if ('syl', 1) in seg[1].items() and len(a_pairs) > 2:
                 # Contains a vowel
-                error = 'epenthesis'            
-                for seg in a_segs:
-                    if ('syl', 1) in a_dict[seg].items():
-                        continue
-                    else:
-                        if seg in t_segs:
-                            if t_segs.index(seg) == 0:
-                                error = error+'-C1pres'
-                            if t_segs.index(seg) == 1:
-                                error = error+'-C2pres'
-                            if t_segs.index(seg) == 2:
-                                error = error+'-C3pres'    
-                        else:        
-                            index = 0
-                            smallest_dist = (index, 1)
-                            for t_ft in t_fts:                        
-                                dist = t_ft-a_dict[seg]                            
-                                if dist < smallest_dist[1]:
-                                    if t_segs[index] not in a_segs:
-                                        smallest_dist = (index, dist)
-                                index += 1
-                            if smallest_dist[0] == 0:
-                                error = error+'-C1sub'
-                            if smallest_dist[0] == 1:
-                                error = error+'-C2sub'
-                            if smallest_dist[0] == 2:
-                                error = error+'-C3sub'                        
-                error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))     
-        
+                error = 'epenthesis'                    
         # Cluster Reduction
         if len(a_segs) < len(t_segs):
             error = "reduction"
-            for seg in a_segs:
-                if seg in t_segs:
-                    if t_segs.index(seg) == 0:
-                        error = error+'-C1pres'
-                    if t_segs.index(seg) == 1:
-                        error = error+'-C2pres'
-                    if t_segs.index(seg) == 2:
-                        error = error+'-C3pres'
-                else:        
-                    index = 0
-                    smallest_dist = (index, 1)
-                    for t_ft in t_fts:                        
-                        dist = t_ft-a_dict[seg]                            
-                        if dist < smallest_dist[1]:
-                            smallest_dist = (index, dist)
-                        index += 1
-                    if smallest_dist[0] == 0:
-                        error = error+'-C1sub'
-                    if smallest_dist[0] == 1:
-                        error = error+'-C2sub'
-                    if smallest_dist[0] == 2:
-                        error = error+'-C3sub'
-            if 'C1' not in error:
-                error = error+'-C1del'                        
-            if 'C2' not in error:
-                error = error+'-C2del'
-            if 'C3' not in error:
-                error = error+'-C3del'            
-            error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
-            return error
-        
         # Substitution
         if len(a_segs) == len(t_segs):
-            error = 'substitution'
-            for seg in a_segs:
-                if seg in t_segs:
-                    if t_segs.index(seg) == 0:
-                        error = error+'-C1pres'
-                    if t_segs.index(seg) == 1:
-                        error = error+'-C2pres'
-                    if t_segs.index(seg) == 2:
-                        error = error+'-C3pres'
-
-                else:        
+             error = 'substitution'
+        # By-Segment Errors
+        if error in ['epenthesis', 'reduction', 'substitution']:
+            for seg in a_pairs:
+                # Skip vowels
+                if ('syl', 1) in seg[1].items() and len(a_pairs) > 2:
+                    continue   
+                
+                if seg[0] in t_segs:
+                    # If segment re-occurs in cluster (MAY NOT NEED THIS IF STATEMENT)
+                    if a_segs.count(seg[0]) > 1:
+                        if t_segs.index(seg[0]) == 0: # and 'C1' not in error:
+                            error = error+'-C1pres'
+                        if t_segs.index(seg[0]) == 1: # and 'C2' not in error:
+                            error = error+'-C2pres'
+                        if t_segs.index(seg[0]) == 2: # and 'C3' not in error:
+                            error = error+'-C3pres'
+                    # All other cases
+                    else:
+                        if t_segs.index(seg[0]) == 0:
+                            error = error+'-C1pres'
+                        if t_segs.index(seg[0]) == 1:
+                            error = error+'-C2pres'
+                        if t_segs.index(seg[0]) == 2:
+                            error = error+'-C3pres'                                                        
+                else:
                     index = 0
                     smallest_dist = (index, 1)
                     for t_ft in t_fts:                        
-                        dist = t_ft-a_dict[seg]                            
-                        if dist < smallest_dist[1]:
-                            if t_segs[index] not in a_segs:
-                                smallest_dist = (index, dist)
+                        dist = t_ft-seg[1]                            
+                        # doesn't count equal segments in distance
+                        if 0 < dist < smallest_dist[1]:
+                            smallest_dist = (index, dist)
                         index += 1
-                    if smallest_dist[0] == 0:
+                    if smallest_dist[0] == 0: # and 'C1' not in error:
                         error = error+'-C1sub'
-                    if smallest_dist[0] == 1:
-                        error = error+'-C2sub'
-                    if smallest_dist[0] == 2:
-                        error = error+'-C3sub'                        
-            error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
+                    if smallest_dist[0] == 1:# and 'C2' not in error:
+                        error = error+'-C2sub' 
+                    if smallest_dist[0] == 2:# and 'C3' not in error:
+                        error = error+'-C3sub'
+
+            
+            ### CHECK FOR multiple assignments to a single target consonant
+            for i in range(len(t_segs)):
+                if error.count(f'C{i+1}') > 1:
+                    # DEBUG
+                    if debug:
+                        error = 'OTHER_'+error
+                    else:
+                        error = error.split('-')[0]+'_other'
+                    return error
+            # RIGHT NOW THIS ADDS "DEL" WHEN multiple assignments made
+            if error == 'substitution_other':
+                pass
+            else:
+                for i in range(len(t_segs)):
+                    if f'C{i+1}' not in error:
+                        error = error+f'-C{i+1}del'
+                   
+
+            # Removes duplicates right now.
+            #error = error.split('-')[0]+'-'+'-'.join(sorted(set(error.split('-')[1:])))
+            error = error.split('-')[0]+'-'+'-'.join(sorted(error.split('-')[1:]))           
             return error
         
         # All other errors
@@ -357,8 +251,9 @@ def error_pattern(target, actual):
         structure = "CCC+"
         print("Only C, CC, CCC are valid targets. CCC+ targets skipped")
 
-def error_quantifier(x, correct_value=0.5, substitution_value=0.3, 
-                     epenthesis_penalty=-0.3):
+def error_quantifier(x, full_correct_value=1, full_deletion_value=0, 
+                     full_substitution_value=0.6, correct_seg_value=1, 
+                     substitution_seg_value=0.6, epenthesis_penalty=-0.3):
     """
     Generates a float value quantifying phonological error patterns
         generated by error_patterns(). Currently used for C and CC clusters, 
@@ -367,43 +262,48 @@ def error_quantifier(x, correct_value=0.5, substitution_value=0.3,
     Args:
         x : str error pattern input in format "pattern"+*"-C#pattern",
             generated by error_pattern()
-        correct_value: float score added for a correct segment in a cluster. Default =
+        correct_seg_value: float score added for a correct segment in a cluster. Default =
             0.5
-        substitution_value : float score added for a substituted segment.
+        substitution_seg_value : float score added for a substituted segment.
             Default = 0.
         epenthesis_penalty : float negative value added to an epenthesized
             segment. Default = -0.3
     
-    Returns float error pattern value.
+    Returns float error pattern score.
     """
     x_list = [x.split('-')[0], x.split('-')[1:]]
+    x_len = len(x_list[1])
     score = 0
-    if len(x_list[1]) > 2:
-        print(f"WARNING: {x} suggests CC+ cluster not validated with this fx")
+    if x_len == 0:
+        x_len = 1
     # Accurate singleton or cluster
     if x == 'accurate':
-        score = 1
+        score = full_correct_value
         return score
     # Deleted singleton or cluster
     if x == 'deletion':
-        score = 0
-        return score
+        score = full_deletion_value
+        return full_deletion_value
     # Epenthesis in a cluster
     if x_list[0] == 'epenthesis':
         score += epenthesis_penalty
     # Substituted singleton
     if x == 'substitution' :
-        score += 0.5
+        score += full_substitution_value
+        return score
+    # Miscellaneous cluster substitution
+    if x == 'substitution_other':
+        score += full_substitution_value
         return score
     # For consonant clusters, allocate points per segment   
     for seg in x_list[1]:
         if 'pres' in seg:
-            score += correct_value/
+            score += correct_seg_value/(x_len*correct_seg_value)
         if 'sub' in seg:
-            score += substitution_value
+            score += substitution_seg_value/(x_len*correct_seg_value)
     return score
 
-def error_patterns_table(input_filename):
+def error_patterns_table(input_filename, score_column=True):
     """
     Generates error patterns for a dataset of transcriptions.
     
@@ -439,13 +339,53 @@ def error_patterns_table(input_filename):
     output_filename = 'error_patterns.csv'
     error_patterns_df = error_patterns_df.merge(error_patterns_series, left_index=True, right_index=True)
     error_patterns_df['error_basic'] = error_patterns_df['error_pattern'].apply(lambda x: x.split('-')[0])
-    error_patterns_df['error_quant'] = error_patterns_df['error_pattern'].apply(error_quantifier)
+    if score_column:
+        error_patterns_df['error_score'] = error_patterns_df['error_pattern'].apply(error_quantifier)
     error_patterns_df.to_csv(output_filename, encoding='utf-8', index=False, na_rep='')
     print(f"Error patterns saved to {output_filename}")
     return error_patterns_df
 
+def import_test_cases(test_cases='test_cases.txt'):
+    """From a txt file, import a list of test cases."""
+    
+    test_cases = []
+    with io.open('test_cases.txt', encoding='utf-8', mode="r") as f:
+        txt = f.readlines()    
+    txt = [line.strip() for line in txt]
+    txt = [line for line in txt if line]
+    epenthesis_other = txt[txt.index('epenthesis_other:')+1:txt.index('other:')]
+    epenthesis_other = [x.split('	') for x in epenthesis_other]
+    test_cases.append(['epenthesis_other', epenthesis_other])
+    
+    other = txt[txt.index('other:')+1:txt.index('reduction_other:')]
+    other = [x.split('	') for x in other]
+    test_cases.append(['other', other])
+    
+    reduction_other = txt[txt.index('reduction_other:')+1:txt.index('substitution_other:')]
+    reduction_other = [x.split('	') for x in reduction_other]
+    test_cases.append(['reduction_other', reduction_other])
 
+    substitution_other = txt[txt.index('substitution_other:')+1:]
+    substitution_other = [x.split('	') for x in substitution_other]
+    test_cases.append(['substitution_other', substitution_other])
+    
+    return test_cases
+    
+def debug_testing(test_cases_list):
+    """from result of import_text_case() get error_pattern() results."""
+    for group in test_cases_list:
+        group.append([])
+        for item in group[1]:
+            group[2].append(error_pattern(item[0], item[1], debug=True))
+            result_list = [[x[0], x[1]] for x in zip(group[1], group[2])]
+        group.append(result_list)
+    return test_cases_list
+           
 
-        
+result = error_patterns_table("G:\My Drive\Phonological Typologies Lab\Projects\Spanish SSD Tx\Data\Processed\ICPLA 2020_2021\SpTxR\microdata_c.csv")
+#result = import_test_cases()
+#result = debug_testing(result)
+
+# Debug Testing
 
 
