3f44f5c2b0cebf234b309df34c827f27ccc898d9
philcombiths <philcombiths@gmail.com>
2020-08-06 17:27:40 -0700

    Project structure adjustments
    Still in progress structure changes

---
+++ error_patterns/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+#from .error_patterns import diacritics
+#from .error_patterns import ph_element
+#from .error_patterns import error_patterns
--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
 	version='0.1',
 	packages=['error_patterns',],
 	license='Apache License, Version 2.0',
-	long_description=open('README.md').read(),
+	long_description=open('README.txt').read(),
 	author='Philip Combiths'
 )
